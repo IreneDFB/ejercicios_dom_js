a3e1a8dfad517ed96aff553d3f274b935b70e0b2
IreneDFB <irene_dfb@hotmail.com>
2025-07-18 20:27:23 +0200

    ejercicio 1 entero

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,2 +1,17 @@
+//1.1
 const boton = document.querySelector('.showme')
 console.log(boton)
+//1.2
+const h1Pillado = document.querySelector('#pillado')
+console.log(h1Pillado)
+//1.3
+const parrafos = document.querySelectorAll('p')
+console.log(parrafos)
+//1.4
+const pokemons = document.querySelectorAll('.pokemon')
+console.log(pokemons)
+//1.5
+const elementosTestMe = document.querySelectorAll('[data-function="testMe"]')
+console.log(elementosTestMe)
+//1.6
+console.log(elementosTestMe[2])
