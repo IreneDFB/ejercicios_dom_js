480a3575faddafcf57a38bef5e400f6926313e1c
IreneDFB <irene_dfb@hotmail.com>
2025-07-28 00:07:51 +0200

    ej 3

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,3 +1,4 @@
+//1.1
 const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela']
 const container = document.querySelector('div[data-function="printHere"]')
 const ul = document.createElement('ul')
@@ -7,3 +8,36 @@ countries.forEach((country) => {
   ul.appendChild(li)
 })
 container.appendChild(ul)
+//1.2
+const elementToRemove = document.querySelector('.fn-remove-me')
+if (elementToRemove) {
+  elementToRemove.remove()
+}
+//1.3
+const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola']
+const carUl = document.createElement('ul')
+cars.forEach((car) => {
+  const li = document.createElement('li')
+  li.textContent = car
+  carUl.appendChild(li)
+})
+container.appendChild(carUl)
+//1.4
+const countries = [
+  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1' },
+  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2' },
+  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3' },
+  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4' },
+  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5' }
+]
+countries.forEach((country) => {
+  const div = document.createElement('div')
+  const h4 = document.createElement('h4')
+  h4.textContent = country.title
+  const img = document.createElement('img')
+  img.src = country.imgUrl
+  img.alt = country.title
+  div.appendChild(h4)
+  div.appendChild(img)
+  document.body.appendChild(div)
+})
